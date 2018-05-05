880f39030d792c45917d48294fa6696d68ca7336
AltmanMe <autumn5816@gmail.com>
2018-05-05 22:28:27 +0800

    delete >

--- predict.py
+++ predict.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
     with tf.Session() as sess:
         encoder_inputs, decoder_inputs, target_weights, outputs, loss, update, saver, learning_rate_decay_op, learning_rate = get_model(feed_previous=True)
         saver.restore(sess, './model/demo')
-        sys.stdout.write("> ")
+#        sys.stdout.write("> ")
         sys.stdout.flush()
 #        input_seq = sys.stdin.readline()
         input_seq = sys.argv[1]
